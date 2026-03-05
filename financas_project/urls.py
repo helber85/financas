7d237767379dd94bb46bab3df73d4f26cb097d94
Helber <helber.chiavegatti@gmail.com>
2026-03-05 16:01:21 -0300

    Ajuste

--- financas_project/urls.py
+++ financas_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 import views
 
@@ -68,3 +70,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('financas_app.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
