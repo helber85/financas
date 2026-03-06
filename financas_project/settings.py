8954c6d00c7e8270b5cf090155e75a6a998153f6
Helber <helber.chiavegatti@gmail.com>
2026-03-06 15:14:24 -0300

    Ajuste

--- financas_project/settings.py
+++ financas_project/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'financas_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "financas_db",
+        "USER": "financas_user",
+        "PASSWORD": "financas_pass",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
